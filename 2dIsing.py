374a004cf5d99c68c81b9965c1497a0abe1f6129
Jeff Crocker <crockj72@gmail.com>
2014-11-22 23:18:09 -0800

    Added status bar for model computation

--- 2dIsing.py
+++ 2dIsing.py
@@ -39,11 +39,16 @@ ax.set_yticks([])
 
 images=ax.imshow(H,cmap=plt.cm.gray, interpolation='gaussian')
 
+def update_progress(amtDone):
+    print "\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(amtDone * 50), amtDone * 100),
+    sys.stdout.flush()
+
 def updatefig(n):
     images.set_array(mcs(H))
+    update_progress(float(n)/NMCS)
     return images,
 
 an = anim.FuncAnimation(fig,updatefig,np.arange(0,NMCS),interval=20,blit=True)
-an.save("ising_L_"+str(L)+"_T_"+str(T)+".mp4",fps=10,extra_args=['-vcodec','libx264'])
+an.save("ising_L_"+str(L)+"_T_"+str(T)+".mp4",fps=30,extra_args=['-vcodec','libx264'])
 
 plt.show()
